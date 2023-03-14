66c475e7ee1f1fc050bb95d6fa54336b116ede4f
joq62 <jakim.leche@gmail.com>
2023-03-14 14:28:33 +0100

    ud

---
+++ cluster_spec.erl
@@ -0,0 +1,51 @@
+-module(cluster_spec).
+
+-export([
+	 start/0,
+	 %% Support
+	 all_files/0,
+	 all_info/0
+	]).
+
+
+-define(Dir,".").
+-define(FileExt,".spec").
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+    
+    check(all_info()),
+    init:stop(),
+    ok.
+
+check([])->
+    io:format("Success, OK ! ~n");
+check([{ok,[{cluster_spec,_Id,Info}]}|T])->
+    io:format("Checking ~p~n",[Info]),
+  
+    true=proplists:is_defined(cookie,Info),
+    true=proplists:is_defined(root_dir,Info),
+    true=proplists:is_defined(pods,Info),
+
+    check(T).
+
+   
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+all_files()->
+    {ok,Files}=file:list_dir(?Dir),
+    FileNames=[filename:join(?Dir,Filename)||Filename<-Files,
+					     ?FileExt=:=filename:extension(Filename)],
+    FileNames.    
+all_info()->
+    [file:consult(File)||File<-all_files()].
+	    
+    
--- cluster_test.spec
+++ cluster_test.spec
@@ -1,6 +1,6 @@
 {cluster_spec,"cluster_test",
  [{cookie,"cookie_cluster_test"},
   {root_dir,"cluster_test"},	
-  {pods,[{3,"cluster_test"}]}
+  {pods,[{3,"c200"},{3,"c201"}]}
  ]
 }.
